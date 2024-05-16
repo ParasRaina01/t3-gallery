d7c37acbfe5158d69e5cfef37f9d2d5be72486c9
GrimReaper <parasraina.33@gmail.com>
2024-05-16 23:38:42 +0530

    postgres set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/c95feef6-62a6-425e-8718-b8c0d08402b1-o5qo3k.jpeg",
+  "https://utfs.io/f/c95feef6-62a6-425e-8718-b8c0d08402b1-o5qo3k.jpeg",
+  "https://utfs.io/f/c95feef6-62a6-425e-8718-b8c0d08402b1-o5qo3k.jpeg",
+  "https://utfs.io/f/c95feef6-62a6-425e-8718-b8c0d08402b1-o5qo3k.jpeg"
+]
 
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery in progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))
+        }
+      </div>
     </main>
   );
 }
